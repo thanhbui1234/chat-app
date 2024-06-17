84580f15b1c4dc63cfd0b75adb88f43fb89c638d
ThanhBC <thanhbc@vnext.vn>
2024-06-17 17:02:52 +0700

    using image assets

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -22,7 +22,7 @@ const Chat = () => {
         <div className="flex gap-3">
           <img
             className="rounded-full w-[25%]"
-            src={"/dist/avatar.jpg"}
+            src={"/dist/assets/avatar.jpg"}
             alt="BigCo Inc. logo"
           />
           <div className="items-center">
--- src/components/list/chatlists/userChat/index.tsx
+++ src/components/list/chatlists/userChat/index.tsx
@@ -40,7 +40,7 @@ const UserChat = () => {
     <div className={cslx('flex flex-col gap-5', style.userlistContainer)}>
       {fakeData.map((user: Data, index: number) => {
         return <div className="flex gap-5 border-b-[1px] border-gray-500 pb-3" key={index}>
-          <img className="rounded-full w-[15%]" src={'/dist/avatar.jpg'} alt={user.name} />
+          <img className="rounded-full w-[15%]" src={'/dist/assets/avatar.jpg'} alt={user.name} />
           <div className="flex-col gap-5">
             <h2>{user.name}</h2>
             <p>{user.msg}</p>
