2186fae89f56b114f97885a85c2341de82ea3328
thanhbc <thanhbc@vnext.vn>
2024-06-20 23:58:21 +0700

    a

--- src/pages/Auth/login.tsx
+++ src/pages/Auth/login.tsx
@@ -1,9 +1,43 @@
+import { useForm, Resolver } from "react-hook-form"
 
-
-const login = () => {
-  return (
-    <div>login</div>
-  )
+type FormValues = {
+  firstName: string
+  lastName: string
 }
 
-export default login
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.firstName ? values : {},
+    errors: !values.firstName
+      ? {
+        firstName: {
+          type: "required",
+          message: "This is required.",
+        },
+      }
+      : {},
+  }
+}
+
+export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver })
+  const onSubmit = handleSubmit((data) => console.log(data))
+
+  return (
+    <form className="mx-auto flex justify-center items-center flex-col gap-10" onSubmit={onSubmit}>
+      <h2 className="text-2xl">Hello my friend</h2>
+      <div className="relative">
+        <input className="text-white p-2 outline-none rounded-sm bg-[#182e43]" {...register("firstName")} placeholder="Email" />
+        {errors?.firstName && <p className="absolute top-10">{errors.firstName.message}</p>}
+      </div>
+
+      <input className=" p-2  text-white rounded-sm outline-none bg-[#182e43]" {...register("lastName")} placeholder="Password" />
+
+      <button className="bg-blue-500 w-48 p-2 rounded-sm">SignIn</button>
+    </form>
+  )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import MainLayout from "../Layouts";
+import LoginPage from "../pages/Auth/login";
+import RegisterPage from "../pages/Auth/register";
 const Router = () => {
   return (
     <>
       <Routes>
-      <Route  path="/" element={<MainLayout />}>
-      <Route  path="/dashboard" element={<Home></Home>}/>
-      </Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route index path="/login" element={<LoginPage></LoginPage>} />
+          <Route index path="/register" element={<RegisterPage></RegisterPage>} />
+          <Route path="/dashboard" element={<Home></Home>} />
+        </Route>
       </Routes>
     </>
   );
